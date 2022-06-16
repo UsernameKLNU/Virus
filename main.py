04bf472a61345e00649118cb5ca2f6f8e9226605
UsernameKLNU <101550757+UsernameKLNU@users.noreply.github.com>
2022-06-16 22:38:51 +0800

    git add main.py git commit git push https://github.com/UsernameKLNU/Virus.git

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import colorama
-import sys
 import platform
-import os
+import sys
+import time
+
+import colorama
+
 
 def MakeError(error_show, exit_num):
     print(colorama.Fore.RED + error_show + colorama.Fore.RESET)
@@ -34,6 +36,24 @@ def TestPlatform():
     print("Virus 版本：" + "1.0.0")
     print(colorama.Fore.RESET)
 
+
 TestPlatform()
 
-input("确定启动病毒 Virus 1.0.0 吗？ >")
+start = input("确定启动病毒 Virus 1.0.0 吗？(Y / n) >")
+if start == "Y":
+    print('你清楚你自己在做什么吗？\n这个程序可能有毁灭电脑核心文件！', end='')
+    real_start = input("(Y / n) > ")
+    if start == "Y":
+        print('请确保电脑已做好备份，或不在实体机内运行本程序。正在准备 Virus ...', end='')
+        time.sleep(10)
+        print("(按 1 并按回车启动 Virus)", end='')
+        virus_start = input()
+        if virus_start == "1":
+        # 病毒要干什么... shh
+        else:
+            MakeError("""
+            Traceback (most recent call last):
+            File \"""" + __file__ + """\", line 48, in <module>
+                        if virus_start == "1":
+            SelectException: selected a not in list thing
+            """, 0xffffffff)
